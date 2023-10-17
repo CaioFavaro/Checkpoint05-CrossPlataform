f118778f6f03b7df4064571b13d9342c856a9940
caio favaro de oliveira <caartoonn86@gmail.com>
2023-10-17 20:50:29 -0300

    Mudanças no metodo de lista
    Foi feito um map para a busca de imagens, ainda falta a tela de usuario.

--- lib/pages/dashboard_page.dart
+++ lib/pages/dashboard_page.dart
@@ -1,33 +1,16 @@
 import 'package:expense_tracker/components/user_drawer.dart';
 import 'package:flutter/material.dart';
+import '../models/DashBoardItems.dart';
 
 class DashBoardPage extends StatefulWidget {
-  const DashBoardPage({super.key});
+  const DashBoardPage({Key? key}) : super(key: key);
 
   @override
-  State<DashBoardPage> createState() => _DashBoardPageState();
+  _DashBoardPageState createState() => _DashBoardPageState();
 }
 
 class _DashBoardPageState extends State<DashBoardPage> {
   var height, width;
-
-  List imgData = [
-    "images/pix.png",
-    "images/consorcio.png",
-    "images/investimento.png",
-    "images/emprestimo.png",
-    "images/seguro.png",
-    "images/pagamento.png",
-  ];
-
-  List titles = [
-    "Pix",
-    "Consorcio",
-    "Investimento",
-    "Emprestimo",
-    "Seguro",
-    "Pagamento"
-  ];
 
   @override
   Widget build(BuildContext context) {
@@ -39,93 +22,99 @@ class _DashBoardPageState extends State<DashBoardPage> {
       ),
       drawer: const UserDrawer(),
       body: Container(
-          color: Colors.purple,
-          height: height,
-          width: width,
-          child: Column(
-            children: [
-              Container(
-                decoration: BoxDecoration(),
-                height: height * 0.300,
-                width: width,
-                child: const Column(
-                  children: [
-                    CircleAvatar(
-                      backgroundImage: NetworkImage('images/icone.png'),
+        color: Colors.purple,
+        height: height,
+        width: width,
+        child: Column(
+          children: [
+            Container(
+              decoration: BoxDecoration(),
+              height: height * 0.225,
+              width: width,
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      top: 20,
+                      left: 15,
+                      right: 15,
                     ),
-                    Padding(
-                      padding: EdgeInsets.only(
-                        top: 20,
-                        left: 15,
-                        right: 15,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Text(
+                          "DashBoard",
+                          style: TextStyle(
+                            fontSize: 30,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w500,
+                            letterSpacing: 1,
+                          ),
+                        ),
+                        Text(
+                          "Ultima atualização 21 Outubro 2023",
+                          style: TextStyle(
+                            fontSize: 15,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w100,
+                            letterSpacing: 1,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              child: GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 4,
+                  childAspectRatio: 1.1,
+                  mainAxisSpacing: 15,
+                ),
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                itemCount: DashBoardItems.itemData.length,
+                itemBuilder: (context, index) {
+                  final title = DashBoardItems.itemData.keys.elementAt(index);
+                  final imagePath = DashBoardItems.itemData[title];
+
+                  return InkWell(
+                    onTap: () {},
+                    child: Container(
+                      margin: const EdgeInsets.symmetric(
+                        vertical: 2,
+                        horizontal: 20,
+                      ),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Colors.white,
+                        boxShadow: const [
+                          BoxShadow(
+                            color: Colors.black,
+                            spreadRadius: 1,
+                            blurRadius: 6,
+                          ),
+                        ],
                       ),
                       child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
                         children: [
-                          Text(
-                            "DashBoard",
-                            style: TextStyle(
-                              fontSize: 30,
-                              color: Colors.white,
-                              fontWeight: FontWeight.w500,
-                              letterSpacing: 1,
-                            ),
-                          ),
-                          Text(
-                            "Ultima atualização 21 Outubro 2023",
-                            style: TextStyle(
-                              fontSize: 15,
-                              color: Colors.white,
-                              fontWeight: FontWeight.w100,
-                              letterSpacing: 1,
-                            ),
+                          Image.asset(
+                            imagePath!,
+                            width: 150,
+                            alignment: Alignment.bottomCenter,
                           ),
                         ],
                       ),
                     ),
-                  ],
-                ),
+                  );
+                },
               ),
-              Container(
-                child: GridView.builder(
-                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 3,
-                        childAspectRatio: 1.1,
-                        mainAxisSpacing: 15),
-                    shrinkWrap: true,
-                    physics: NeverScrollableScrollPhysics(),
-                    itemCount: 6,
-                    itemBuilder: (context, index) {
-                      return InkWell(
-                        onTap: () {},
-                        child: Container(
-                          margin:
-                              EdgeInsets.symmetric(vertical: 2, horizontal: 20),
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(20),
-                              color: Colors.white,
-                              boxShadow: [
-                                BoxShadow(
-                                  color: Colors.black,
-                                  spreadRadius: 1,
-                                  blurRadius: 6,
-                                )
-                              ]),
-                          child: Column(
-                            children: [
-                              Image.asset(
-                                imgData[index],
-                                width: 150,
-                                alignment: Alignment.bottomCenter,
-                              )
-                            ],
-                          ),
-                        ),
-                      );
-                    }),
-              )
-            ],
-          )),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
