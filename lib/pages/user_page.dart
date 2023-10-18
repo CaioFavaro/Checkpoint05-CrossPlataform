50a05e750cdabd752764bb0264b597257fc54443
caio favaro de oliveira <caartoonn86@gmail.com>
2023-10-18 19:09:00 -0300

    Criação da tela de usuario
    Foi feita a criação da tela de usuario a partir da tela de drawer, ainda falta o visual da tela e povoar o banco de transação.

---
+++ lib/pages/user_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class UserPage extends StatefulWidget {
+  const UserPage({Key? key}) : super(key: key);
+
+  @override
+  _UserPageState createState() => _UserPageState();
+}
+
+class _UserPageState extends State<UserPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('User'),
+      ),
+    );
+  }
+}
